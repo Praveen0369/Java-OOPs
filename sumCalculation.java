2e00a6b0a073788783d0c96e266e06881e616075
Praveen0369 <89630842+Praveen0369@users.noreply.github.com>
2021-09-11 16:09:36 +0530

    Create sumCalculation.java

---
+++ sumCalculation.java
@@ -0,0 +1,9 @@
+public class sumCalculation{
+void sum(int a,int b){System.out.println(a+b);}
+void sum(int a,int b,int c){System.out.println(a+b+c);}
+public static void main(String args[]){
+sumCalculation obj=new sumCalculation();
+obj.sum(10,10,10);
+obj.sum(20,20);
+}
+}  
