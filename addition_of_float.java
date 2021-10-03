a898796f9e51e284c650d812af07d537a6beca8d
Praveen0369 <89630842+Praveen0369@users.noreply.github.com>
2021-10-03 19:16:32 +0530

    Create addition_of_float.java

---
+++ addition_of_float.java
@@ -0,0 +1,15 @@
+//Program To Add Two floating point Numbers
+class addfloat
+{
+  float a=10.f;
+  float b=20.f;
+}
+public class addition_of_float extends addfloat
+{
+    public static void main(String args[])
+    {
+        addition_of_float obj= new addition_of_float();
+        System.out.print("addittion of float:"+obj.a);
+    }
+    
+}
