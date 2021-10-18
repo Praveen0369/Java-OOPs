e3840e35fc03453584839c0943dec8a5c1e40628
Praveen0369 <89630842+Praveen0369@users.noreply.github.com>
2021-10-18 22:46:16 +0530

    Create toprintastrikpyramid.java

---
+++ toprintastrikpyramid.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+ 
+public class toprintastrikpyramid
+{
+
+
+
+    public static void printTriagle(int n)
+
+    {
+
+       
+        for (int i=0; i<n; i++)
+
+       {
+
+            for (int j=n-i; j>1; j--)
+
+            {
+                System.out.print(" ");
+
+            }
+
+            for (int j=0; j<=i; j++ )
+
+            {
+
+                
+
+                System.out.print("* ");
+
+            }
+
+
+            System.out.println();
+
+        }
+
+    }
+
+     
+    public static void main(String args[])
+
+    {
+
+        int n = 5;
+        toprintastrikpyramid o= new toprintastrikpyramid();
+
+        o.printTriagle(n);
+
+    }
+}
