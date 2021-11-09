bcd781add08f97ed7af1f26301107249bcd04a34
Praveen0369 <89630842+Praveen0369@users.noreply.github.com>
2021-11-09 19:19:09 +0530

    Create CalculatorDISteam.java

---
+++ CalculatorDISteam.java
@@ -0,0 +1,48 @@
+import java.io.DataInputStream;
+public class CalculatorDISteam
+{
+public static void main(String args[])
+{
+ DataInputStream pt=new DataInputStream(System.in);
+ int x,y,z,ch;
+
+try
+ {
+ System.out.println("Enter Value of X:");
+ x = Integer.parseInt(pt.readLine());
+ System.out.println("Enter Value of Y:");
+ y = Integer.parseInt(pt.readLine());
+System.out.println("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division");
+System.out.println("Enter ur choice:");
+ch = Integer.parseInt(pt.readLine());
+
+
+ switch(ch)
+ {
+ case 1:
+z = x + y;
+System.out.println("The Addition is:"+z);
+ break;
+case 2:
+z = x - y;
+System.out.println("The Subtractionis:"+z);
+ break;
+case 3:
+z = x * y;
+System.out.println("The Multiplicationis:"+z);
+ break;
+case 4:
+z = x / y;
+System.out.println("The Division is:"+z);
+ break;
+default:
+System.out.println("Sorry TryAgain.........");
+break;
+}
+}
+catch(Exception e)
+{
+System.out.println("x.getmessage()");
+}
+}
+}
